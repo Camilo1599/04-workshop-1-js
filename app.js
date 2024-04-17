39d133932c0092e4b79757669fc46df03dd4a380
Camilo1599 <camimilo2014@gmai.com>
2024-04-17 01:00:40 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+// este es un comentario de una sola linea 
+/*este es un comentario
+de multiples lineas*/
+// const miNumero = 10;
+// console.log(miNumero);
+
+// let miBooleano = true;
+// console.log(typeof miBooleano);
+
+// let miCadena = "Hello World";
+// console.log(miCadena.length)
+
+// const miArray =["Hola Mundo", 123, [1,2]]
+// console.log(miArray)
+
+// const miObjeto = {
+//     name:'camilo',
+//     age:24,
+//     bool:true,
+// }
+// console.log(miObjeto)
+
+// let nombreUsuario = prompt("Por favor ingrese su nombre: ")
+// alert(`hola ${nombreUsuario}`)
+
+let likeJs = confirm("Te gusta JavaScript.")
+if (likeJs == true){
+    console.log("A el Usuario le gusta JavaScript")
+}
+else{
+    console.log("Al usuario no le gusta JavaScript.")
+}
+
+console.warn("Esto es solo practica")
+
+console.error("hay un error de operaciones")
+
+console.table([1, 2, 3, 4, 5])
